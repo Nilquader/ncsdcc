cb62d958abdc13148d5a46e85ba8cbc598ad4b9a
Johannes (R400,Linux) <johannes.rosky@gmx.de>
2011-09-11 16:20:01 +0200

    fixed another selectfile register handling bug

--- libnc100/selectfile.s
+++ libnc100/selectfile.s
@@ -1,6 +1,6 @@
 ; char* selectfile(void);
 _selectfile::
-	push af
+	push af ; af, bc, de, is are destroyed during selectfile system call
 	push bc
 	push de
 	push ix
@@ -8,7 +8,11 @@ _selectfile::
 	pop ix
 	pop de
 	pop bc
-	pop af
-	ret c
-	ld hl, #0000
+	jr c, _selectfile2 	
+	pop af ; restore af
+	ld hl, #0000 ; no file was selected
 	ret
+_selectfile2:: ; selected a file
+	pop af  ; af must be restored in both cases
+	ret
+
